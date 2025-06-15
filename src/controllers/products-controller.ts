48e652f03fd3cae305e0baaa273fc70a15a32794
Alex Lael <alex.lael.bsb@gmail.com>
2025-06-15 18:06:23 -0300

    feat: implement custom error handling with AppError class

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
 
 class ProductsController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
       
-    
+      throw new AppError("This is a custom error message", 400);
       return response.status(200).json({messsage: "ok"});
     } catch (error) {
       next(error);
