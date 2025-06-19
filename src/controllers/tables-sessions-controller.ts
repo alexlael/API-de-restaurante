b65276ea90665b21287b331e9ce947a06b79f6db
Alex Lael <alex.lael.bsb@gmail.com>
2025-06-18 22:42:04 -0300

    feat: implement session creation logic with validation and error handling

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,15 +1,39 @@
 import { NextFunction, Request, Response } from "express";
+import { knex } from "@/database/knex";
+import { z } from "zod";
+import { AppError } from "@/utils/AppError";
+import { table } from "console";
 
 class TablesSessionsController {
-    async create(request: Request, response: Response, next: NextFunction) {
-        try {
-           
-            return response.status(201).json({ message: "Session created" });
-        } catch (error) {
-            next(error);
-        }
-    }
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        table_id: z.number(),
+      });
 
+      const { table_id } = bodySchema.parse(request.body);
+
+      //verificando se a mesa já está aberta
+      const session = await knex<TablesSessionsRespository>("tables_sessions")
+        .where({ table_id })
+        .orderBy("opened_at", "desc")
+        .first();
+
+      if (session && !session.closed_at) {
+        throw new AppError("Mesa já está aberta");
+      }
+
+      // abrindo a mesa
+      await knex<TablesSessionsRespository>("tables_sessions").insert({
+        table_id,
+        opened_at: knex.fn.now(),
+      });
+
+      return response.status(201).json();
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export { TablesSessionsController };
