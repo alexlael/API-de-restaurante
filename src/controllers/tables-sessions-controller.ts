05311ac269e2150db196ab0396ae44054f9a1bec
Alex Lael <alex.lael.bsb@gmail.com>
2025-06-19 00:07:58 -0300

    feat: implement index and update methods in TablesSessionsController with routing

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -34,6 +34,52 @@ class TablesSessionsController {
       next(error);
     }
   }
+
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+      const sessions = await knex<TablesSessionsRespository>(
+        "tables_sessions"
+      ).orderBy("closed_at");
+
+      return response.status(200).json(sessions);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async update(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), {
+          message: "ID inválido",
+        })
+        .parse(request.params.id);
+
+      const session = await knex<TablesSessionsRespository>("tables_sessions")
+        .where({ id })
+        .first();
+
+      if (!session) {
+        throw new AppError("Sessão não encontrada");
+      }
+
+      if (session.closed_at) {
+        throw new AppError("Sessão já foi fechada");
+      }
+
+      await knex<TablesSessionsRespository>("tables_sessions")
+        .update({
+          closed_at: knex.fn.now(),
+        })
+        .where({ id });
+
+      return response.json();
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export { TablesSessionsController };
