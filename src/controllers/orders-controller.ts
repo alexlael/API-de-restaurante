83cd603b1d95b225087487983489da3be089af94
Alex Lael <alex.lael.bsb@gmail.com>
2025-06-20 15:11:59 -0300

    feat: refactor create method in OrdersController for improved readability and structure

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -1,27 +1,42 @@
 import { NextFunction, Request, Response } from "express";
-import {z } from "zod";
+import { z } from "zod";
 import { AppError } from "@/utils/AppError";
+import { knex } from "@/database/knex";
 
-class OrdersController{
-    async create(request: Request, response: Response, next: NextFunction) {
-        try {
+class OrdersController {
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        table_session_id: z.number(),
+        product_id: z.number(),
+        quantity: z.number(),
+      });
 
-            const bodySchema = z.object({
-            table_session_id: z.number(),
-            product_id: z.number(),
-            quantity: z.number()
-            })
+      const { table_session_id, product_id, quantity } = bodySchema.parse(
+        request.body
+      );
 
-            const { table_session_id, product_id, quantity } = bodySchema.parse(request.body);
+      const session = await knex<TablesSessionsRespository>("tables_sessions")
+        .where({ id: table_session_id })
+        .first();
 
-            return response.status(201).json()
-            
-        } catch (error) {
-            next(error);
-            
-        }
+      if (!session) {
+        throw new AppError("Table session not found");
+      }
+
+      if (session.closed_at) {
+        throw new AppError("Table session is already closed");
+      }
+
+      const product = await knex<ProductRepository>("products")
+        .where({ id: product_id })
+        .first();
+
+      return response.status(201).json(product);
+    } catch (error) {
+      next(error);
     }
+  }
 }
-
 
 export { OrdersController };
